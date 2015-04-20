3ee9e2ca2ca10ba5674696205b761cf775d03b9c
Dmitriy Ivanov <dimitry@google.com>
2015-04-20 16:59:05 -0700

    Revert "Pack relocation tables for dynamic executables"
    This reverts commit 4c2d1a64fb49e706a8c7877bb40212ada78d25d7.
    
    Conflicts:
    	core/combo/TARGET_linux-mips.mk
    	core/combo/TARGET_linux-mips64.mk
    	core/combo/TARGET_linux-x86.mk
    	core/dynamic_binary.mk

--- core/dynamic_binary.mk
+++ core/dynamic_binary.mk
@@ -95,7 +95,7 @@ strip_output := $(LOCAL_BUILT_MODULE)
 
 my_strip_module := $(LOCAL_STRIP_MODULE)
 ifeq ($(my_strip_module),)
-  my_strip_module := true
+  my_strip_module := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_STRIP_MODULE)
 endif
 
 $(strip_output): PRIVATE_STRIP := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_STRIP)
@@ -143,5 +143,4 @@ endif # my_strip_module
 
 $(cleantarget): PRIVATE_CLEAN_FILES += \
     $(linked_module) \
-    $(symbolic_output) \
-    $(strip_output)
+    $(symbolic_output)
